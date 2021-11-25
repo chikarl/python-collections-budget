f0f1c664228d0aea7b32adf709cc9ea19289ed79
Chi Karl <kchi8573@gmail.com>
2021-11-25 20:55:40 +0100

    More fix

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -6,8 +6,10 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
-    if divided_set_loop != divided_set_comp:
+    
+    if divided_for_loop != divided_set_comp:
         print('Sets are NOT equal by == test')
+        
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
